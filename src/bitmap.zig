43b7d58415d31c28b5f024d1aeb68def0f10392f
alvarorichard <alvaro10048@protonmail.com>
2023-12-08 02:40:07 -0300

    finish bitmap

--- src/bitmap.zig
+++ src/bitmap.zig
@@ -12,32 +12,50 @@ pub const Bitmap = struct {
 
     //cria bitmapa com tamanho especificado 
     pub fn create(allocator:std.mem.Allocator, width:u8, height:u8) !Self {
-        _ = height;
-        _ = width;
-        _ = allocator;
+        //aloca pixel em array
+        var pixels = try allocator.alloc(u1, @as(u16,width) * @as(u16,height));
+        @memset(pixels, 0);
+
+        return Self{
+            .allocator = allocator,
+            .width = width,
+            .height = height,
+            .pixels = pixels,
+        };
       
     }
 
     // limpa bitmap
 
     pub fn free(self: *Self) void {
-        _ = self;
+        self.allocator.free(self.pixels);
     }
+
+    // limpar bitmap
+     pub fn clear(self: *Self) void {
+        @memset(self.pixels, 0);
+    }
+
 
     // define pixel na posicao especificada
 
     pub fn setPixel(self: *Self, x:u8, y:u8 ) bool {
-        _ = self;
-        _ = x;
-        _ = y;
+
+        if (x >= self.width or y >= self.height) {
+            return false;
+        }
     }
 
     // retorna pixel na posicao especificada
 
     pub fn getPixel(self: *Self, x:u8, y:u8 ) u1 {
-        _ = self;
-        _ = x;
-        _ = y;
+
+        if (x >= self.width or y >= self.height) {
+            return 0;
+        }
+
+        var  index = @as(u16,x) + @as(u16,y) * @as(u16,self.width);
+        return self.pixels[index];
     }
 
 
--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const Display = @import("display.zig").Display;
+const Bitmap = @import("bitmap.zig").Bitmap;
 
 pub fn main() !void {
    
@@ -9,5 +10,18 @@ pub fn main() !void {
    while (display.open) {
        display.input(); 
    }
+
+   var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+   const allocator = gpa.allocator();
+   defer{
+    const leaked = gpa.deinit();
+    if (leaked == .leak) {
+        @panic("VAZAMENTO DE MEMORIA");
+    }
 }
+    var bitmap = try Bitmap.create(allocator,64,32);
+    defer bitmap.free();
+}
+
+
 
