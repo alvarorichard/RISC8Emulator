5ebb079b9f0b022f515bf9872b21c7dcd2e2e769
alvarorichard <alvaro10048@protonmail.com>
2023-12-08 01:40:48 -0300

    display with zls error

---
+++ src/display.zig
@@ -0,0 +1,65 @@
+const std = @import("std");
+const c = @import("c.zig");
+
+pub const Display = struct{
+ 
+const Self = @This();
+
+window: *c.SDL_Window,
+open: bool,
+
+pub fn create(
+    title: []const u8,
+    width: i32,height: i32,
+) !Self {
+    _ = height;
+    _ = width;
+    _ = title;
+    if(c.SDL_Init(c.SDL_INIT_VIDEO | c.SDL_INIT_AUDIO) != 0) {
+  return error.SDLInitializationFailed;
+  var window = c.SDL_CreateWindow(
+  title,
+  c.SDL_WINDOWPOS_UNDEFINED,c.SDL_WINDOWPOS_UNDEFINED,
+  width,height,
+  c.SDL_WINDOW_SHOWN,
+) orelse {
+  c.SDL_Quit();
+  return error.SDLWindowCreationFailed;
+};
+  _ = window;
+  return Self{
+    .window = window,
+    .open = true,
+ };
+}
+
+
+pub fn free(self: *Self) void{
+    c.SDL_DestroyWindow(self.window);
+    c.SDL_Quit();
+
+}
+
+pub fn input (self: *Self) void{
+    var event: c.SDL_Event = undefined;
+    while (c.SDL_PollEvent(&event) != 0) {
+        switch (event.@"type") {
+            c.SDL_QUIT => {
+                self.open = false;
+            },
+            else => {},
+        }
+    }
+}
+}
+};
+
+
+
+
+
+
+
+
+
+
